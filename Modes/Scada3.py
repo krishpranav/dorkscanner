1e2c00d770d6b44c3a6dada1f741682b43482497
krishpranav <krisna.pranav@gmail.com>
2021-04-25 13:27:19 +0530

    updated scada3.py

---
+++ Modes/Scada3.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env/python3
+# -*- coding: utf-8 -*-
+
+#imports
+import sys
+import os
+from googlesearch import search
+from termcolor import colored, cprint
+import random
+from http import cookiejar
+class BlockAll(cookiejar.CookiePolicy):
+    return_ok = set_ok = domain_return_ok = path_return_ok = lambda self, *args, **kwargs: False
+    netscape = True
+    rfc2965 = hide_cookie2 = False
+    
+TLD = ["com.qa","ru","com.sa"]
+
+lux = random.choice(TLD)
+
+Vx = """ inurl:/Portal0000.htm """
+query = Vx
+
+def spinning_cursor():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
+
+spinner = spinning_cursor()
+for _ in range(100):
+    sys.stdout.write(next(spinner))
+    sys.stdout.flush()
+    time.sleep(0.1)
+    sys.stdout.write('\b')
+
+for gamma in search(query, tld=lux, num=10,stop=50,pause=2): 
+     print(colored ('[+] Found > ' ,'yellow')  + (gamma) )  
+print(colored('[+] 90% done ', 'green' ))
+print(colored('[+] 100% done ', 'green' ))
+print(colored('[+] done ', 'green' ))
+print(colored ('[! >] delete .google-cookie file in dork scanner  ' ,'red')) 
