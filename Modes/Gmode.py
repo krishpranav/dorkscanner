b7778d0e4b3f80636fa886437f70dc3bda2c378d
krishpranav <krisna.pranav@gmail.com>
2021-04-25 10:52:07 +0530

    added imports and block all class

--- Modes/Gmode.py
+++ Modes/Gmode.py
@@ -9,3 +9,7 @@ from termcolor import colored, cprint
 import warnings
 import random
 from http import cookiejar
+class BlockAll(cookiejar.CookiePolicy):
+    return_ok = set_ok = domain_return_ok = path_return_ok = lambda self, *args, **kwargs: False
+    netscape = True
+    rfc2965 = hide_cookie2 = False
