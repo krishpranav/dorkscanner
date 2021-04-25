a6ce1a245787171cf7c2f87a5aa5a6a349fa25b4
krishpranav <krisna.pranav@gmail.com>
2021-04-25 10:47:25 +0530

    updated

---
+++ Modes/Gmode.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env/python3
+
+#imports
+import requests
+import proxybroker
+from googlesearch import search
+import sys
+from termcolor import colored, cprint
+import warnings
+import random
+from http import cookiejar
