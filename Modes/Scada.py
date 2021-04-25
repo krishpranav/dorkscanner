46ffb824b0a7b169f5ab0846af27359bfb0f5e14
krishpranav <krisna.pranav@gmail.com>
2021-04-25 13:17:17 +0530

    updated cursor

--- Modes/Scada.py
+++ Modes/Scada.py
@@ -39,5 +39,46 @@ __/_____|___||__|||___|____|__________________________________________
 
 print(NPP)
 
+
 TLD = ["com","com.tw","co.in"]
 beta = random.choice(TLD)
+betax = random.choice(TLD)
+print (" ")
+print(colored('[+] Searching... ', 'green')) 
+B =  """ intitle:"Rockwell Automation" "Device Name" "Uptime" """
+query = B
+# ****
+def spinning_cursor():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
+
+spinner = spinning_cursor()
+for _ in range(100):
+    sys.stdout.write(next(spinner))
+    sys.stdout.flush()
+    time.sleep(0.1)
+    sys.stdout.write('\b')
+#*****
+for gamma in search(query, tld=beta,stop=50, num=10,pause=2): 
+  print(colored ('[+] Found > ' ,'yellow')  + (gamma) )
+print(colored('[+] 20% done ', 'green')) 
+B = """ inurl:dtm.html intitle:1747-L551 """
+query = B
+# ****
+def spinning_cursor():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
+
+spinner = spinning_cursor()
+for _ in range(100):
+    sys.stdout.write(next(spinner))
+    sys.stdout.flush()
+    time.sleep(0.1)
+    sys.stdout.write('\b')
+#*****
+for gamma in search(query, tld=betax, num=10,stop=50,pause=2): 
+     print(colored ('[+] Found > ' ,'yellow')  + (gamma) )     
+print(colored('[+] 40% done ', 'green' )) # more scada dorks will be added here
+from Modes import Scada2   
