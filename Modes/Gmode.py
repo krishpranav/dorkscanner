e1ec6a0deb6046263ace31113478cada1b85bb20
krishpranav <krisna.pranav@gmail.com>
2021-04-25 10:54:48 +0530

    updated Gmode.py

--- Modes/Gmode.py
+++ Modes/Gmode.py
@@ -13,3 +13,31 @@ class BlockAll(cookiejar.CookiePolicy):
     return_ok = set_ok = domain_return_ok = path_return_ok = lambda self, *args, **kwargs: False
     netscape = True
     rfc2965 = hide_cookie2 = False
+
+def fxn():
+    warnings.warn("deprecated", DeprecationWarning)
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    fxn()
+
+def clear():
+    return os.system('cls' if os.name == 'nt' else 'clear')
+
+print ("")
+
+A = "GOOGLE DORK SCANNER"
+print ("")
+print(A)
+TLD = ["com","com.tw","co.in","be","de","co.uk","co.ma","dz","ru","ca"]
+s = requests.Session()
+s.cookies.set_policy(BlockAll())
+alpha = input (colored('[>] Please set a Dork : ', 'green' )) 
+query = alpha
+beta =  random.choice(TLD)   
+
+    
+for gamma in search(query, tld=beta, num=10 , stop=95 , pause=2): 
+    print(colored ('[+] Found > ' ,'yellow')  + (gamma) )
+print(colored ('[+] Done  ' ,'green'))
+print(colored ('[! >] delete .google-cookie file in Katana DIR  ' ,'red')) 
